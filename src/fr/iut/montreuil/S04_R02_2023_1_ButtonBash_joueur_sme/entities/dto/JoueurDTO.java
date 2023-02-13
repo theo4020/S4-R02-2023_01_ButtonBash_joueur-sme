ba4fbddb5ababf1fc755f0737e8dd872f74f6e17
Tarook <Tarook.Zinou@gmail.com>
2023-02-13 20:23:55 +0100

    Added libraries and DTO

---
+++ src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/entities/dto/JoueurDTO.java
@@ -0,0 +1,24 @@
+package fr.iut.montreuil.S04_R02_2023_1_ButtonBash_joueur_sme.entities.dto;
+
+public class JoueurDTO {
+    private String pseudo;
+    private int score;
+
+    public JoueurDTO(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    //getters and setters
+    public String getPseudo() {
+        return pseudo;
+    }
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
